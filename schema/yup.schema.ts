477d62faaf0e8ca60286df40807d4b6321d0c667
Exequiel <exe2016gerez@gmail.com>
2023-12-03 02:00:36 -0300

    refactor: remove formik, yup dependencies & schema directory

--- schema/yup.schema.ts
+++
@@ -1,7 +0,0 @@
-import * as Yup from "yup";
-
-export const validationSchema = Yup.object().shape({
-    senderEmail: Yup.string().email("Ingrese un correo electrónico válido").required("Este campo es obligatorio"),
-    message: Yup.string().required("Este campo es obligatorio"),
-  });
-  
