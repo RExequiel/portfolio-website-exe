a65ee4a30695cff5b4d63a84edfdcfda6419f51c
Exequiel <exe2016gerez@gmail.com>
2023-12-03 06:06:56 -0300

    feat: add button in projects section

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,6 +5,7 @@ import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
+import Link from "next/link";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
@@ -18,6 +19,14 @@ export default function Projects() {
             <Project {...project} />
           </React.Fragment>
         ))}
+      </div>
+      <div className="mt-8 text-center">
+        <Link
+          href="https://github.com/RExequiel"
+          className="group bg-gray-900 text-white px-7 py-3 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Ver más proyectos
+        </Link>
       </div>
     </section>
   );
