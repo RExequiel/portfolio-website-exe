2da0395e3f84064f630ab77c0362827ffa4bbf63
Exequiel <exe2016gerez@gmail.com>
2023-12-06 20:51:31 -0300

    refactor: remove link (download CV English)

--- components/intro.tsx
+++ components/intro.tsx
@@ -93,18 +93,10 @@ export default function Intro() {
           href="/CV-Exequiel-Gerez-Español.pdf"
           download
         >
-          Descargar CV Español{" "}
+          Descargar CV{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
 
-        <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV-Exequiel-Gerez-English.pdf"
-          download
-        >
-          Download CV English{" "}
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </a>
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://www.linkedin.com/in/exequielgerez"
